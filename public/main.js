44ea33b328bd8583f5774f93c9432effec529442
Justin Swiger <justin.swiger@me.com>
2019-10-14 13:53:30 -0400

    moved function

--- public/main.js
+++ public/main.js
@@ -6,36 +6,34 @@ let playerOneChoice = undefined;
 let playerTwoChoice = undefined;
 
 const comparePlayers => {
-
-}
-
-// if player 1 and 2 tie
-if (playerOneChoice === playerTwoChoice) {
-	return "It's a Tie";
-}
-// if player 1 chooses rock
-if (playerOneChoice === 'rock') {
-	if ((playerTwoChoice = 'scissors')) {
-		return 'Player 1 Wins';
-	} else {
-		return 'Player 2 Wins';
-  }
-}
-// if player 1 chooses scissors
-if (playerOneChoice === 'scissors') {
-	if ((playerTwoChoice = 'paper')) {
-		return 'Player 1 Wins';
-	} else {
-		return 'Player 2 Wins';
+	// if player 1 and 2 tie
+	if (playerOneChoice === playerTwoChoice) {
+		return "It's a Tie";
 	}
-}
-// if player 1 chooses paper
-if (playerOneChoice === 'paper') {
-	if ((playerTwoChoice = 'rock')) {
-		return 'Player 1 Wins';
-	} else {
-		return 'Player 2 Wins';
+	// if player 1 chooses rock
+	if (playerOneChoice === 'rock') {
+		if ((playerTwoChoice = 'scissors')) {
+			return 'Player 1 Wins';
+		} else {
+			return 'Player 2 Wins';
 	}
+	}
+	// if player 1 chooses scissors
+	if (playerOneChoice === 'scissors') {
+		if ((playerTwoChoice = 'paper')) {
+			return 'Player 1 Wins';
+		} else {
+			return 'Player 2 Wins';
+		}
+	}
+	// if player 1 chooses paper
+	if (playerOneChoice === 'paper') {
+		if ((playerTwoChoice = 'rock')) {
+			return 'Player 1 Wins';
+		} else {
+			return 'Player 2 Wins';
+		}
+}
 }
 
 // program to run choices
