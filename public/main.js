0ff81c9c9ec6e856c91419892523de6a69995c39
Justin Swiger <justin.swiger@me.com>
2019-10-14 14:30:11 -0400

    added buttons

--- public/main.js
+++ public/main.js
@@ -5,7 +5,23 @@ const qs = (element) => document.querySelector(element);
 let playerOneChoice = undefined;
 let playerTwoChoice = undefined;
 
-const comparePlayers => {
+// defining buttons
+const pOneRockButton = qs('r');
+const pTwoRockButton = qs('r');
+const pOnePaperButton = qs('p');
+const pTwoPaperButton = qs('p');
+const pOneScissorsButton = qs('s');
+const pTwoScissorsButton = qs('s');
+
+// this isn't working...
+console.log(pOneRockButton);
+const showPlayerOneChoice = qs('.playerOneChoice');
+showPlayerOneChoice.textContent = playerOneChoice;
+const showPlayerTwoChoice = qs('.playerTwoChoice');
+showPlayerTwoChoice.textContent = playerTwoChoice;
+
+// comparing results
+const comparePlayers = () => {
 	// if player 1 and 2 tie
 	if (playerOneChoice === playerTwoChoice) {
 		return "It's a Tie";
@@ -16,7 +32,7 @@ const comparePlayers => {
 			return 'Player 1 Wins';
 		} else {
 			return 'Player 2 Wins';
-	}
+		}
 	}
 	// if player 1 chooses scissors
 	if (playerOneChoice === 'scissors') {
@@ -33,12 +49,12 @@ const comparePlayers => {
 		} else {
 			return 'Player 2 Wins';
 		}
-}
-}
+	}
+};
 
 // program to run choices
 const main = () => {
-	qs('.player-1-rock-button').addEventListener('click', comparePlayers);
+	qs('.player-1-rock-button').addEventListener('click', pOneRockButton);
+	console.log('.pOneRockButton');
 };
-console.log(comparePlayers)
 document.addEventListener('DOMContentLoaded', main);
